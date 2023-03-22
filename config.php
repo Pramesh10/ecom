5299d181746415b773c878b286354aa9ab96f7ca
Pramesh10 <51102609+Pramesh10@users.noreply.github.com>
2023-03-22 21:05:55 +0545

    Add files via upload

---
+++ config.php
@@ -0,0 +1,4 @@
+<?php
+define('UPLOAD_DIR','uploaded_img');
+$conn= mysqli_connect('localhost','root','','artnep') or die('connection failed');
+?>
---
+++ setting.php
@@ -0,0 +1,8 @@
+<?php
+$epay_url="https://uat.esewa.com.np/epay/main";
+$pid="store1000000";
+$failedurl="http://localhost/Ecom/esewa_failed.php?q=fu";
+$successurl="http://localhost/Ecom/esewa_success.php?q=su";
+$merchant_code="epay_payment";
+$fraudcheck_url="https://uat.esewa.com.np/epay/transrec";
+?>
